8a84beaf98e68d549d7a00f7911ee287fbe4d7a9
NeiJunior <Nei Junior@DESKTOP-SS8QBJE>
2024-09-09 19:22:51 -0300

    wipe

--- src/GerenciadorGeral.presentation/GerenciadorGeral.presentation.Client/Program.cs
+++ src/GerenciadorGeral.presentation/GerenciadorGeral.presentation.Client/Program.cs
@@ -1,5 +1,11 @@
+using GerenciadorGeral.application.Servicos;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Reflection.Emit;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+var host = builder.Build();
+host.Services.GetRequiredService<CompraApp>();
+
 
 await builder.Build().RunAsync();
