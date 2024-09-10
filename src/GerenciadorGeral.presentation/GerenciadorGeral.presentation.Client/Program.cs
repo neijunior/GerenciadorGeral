8e1f3de070dbea7fd44706d22a91b29f45a2f197
nei.faria <nei.faria@supplyi.com.br>
2024-09-10 19:46:07 -0300

    wipe

--- src/GerenciadorGeral.presentation/GerenciadorGeral.presentation.Client/Program.cs
+++ src/GerenciadorGeral.presentation/GerenciadorGeral.presentation.Client/Program.cs
@@ -1,11 +1,11 @@
 using GerenciadorGeral.application.Servicos;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using System.Reflection.Emit;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.Register()
 
 var host = builder.Build();
-host.Services.GetRequiredService<CompraApp>();
+
 
 
 await builder.Build().RunAsync();
