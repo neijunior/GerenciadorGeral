13f8d3d8bb7287718fecb4994dbd3cda99927c5c
nei.faria <nei.faria@supplyi.com.br>
2024-09-11 19:01:24 -0300

    wipe

--- src/GerenciadorGeral.infra/GerenciadorGeral.infra.IoC/DependencyInjectionAplicacao.cs
+++ src/GerenciadorGeral.infra/GerenciadorGeral.infra.IoC/DependencyInjectionAplicacao.cs
@@ -4,16 +4,16 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace GerenciadorGeral.infra.IoC
 {
-  public static class DependencyInjectionAplicacao
-  {
-    public static void RegisterAplicacao(this IServiceCollection svcCollection)
+    public static class DependencyInjectionAplicacao
     {
-      svcCollection.AddScoped(typeof(IAppBase<,>), typeof(AppBase<,>));
-      svcCollection.AddScoped<ICompraApp, CompraApp>();
-      svcCollection.AddScoped<IFornecedorApp, FornecedorApp>();
-      svcCollection.AddScoped<IMenuApp, MenuApp>();
-      svcCollection.AddScoped<ISKUApp, SKUApp>();
-      svcCollection.AddScoped<IUnidadeMedidaApp, UnidadeMedidaApp>();
+        public static void RegisterAplicacao(this IServiceCollection svcCollection)
+        {
+            svcCollection.AddScoped(typeof(IAppBase<,>), typeof(AppBase<,>));
+            svcCollection.AddScoped<ICompraApp, CompraApp>();
+            svcCollection.AddScoped<IFornecedorApp, FornecedorApp>();
+            svcCollection.AddScoped<IMenuApp, MenuApp>();
+            svcCollection.AddScoped<ISKUApp, SKUApp>();
+            svcCollection.AddScoped<IUnidadeMedidaApp, UnidadeMedidaApp>();            
+        }
     }
-  }
 }
